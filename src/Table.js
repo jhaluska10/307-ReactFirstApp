975d2ce5847ec56b0e515ae484d738d410f39099
Joseph H <jhaluska@calpoly.edu>
2022-01-08 13:02:03 -0800

    added form

--- src/Table.js
+++ src/Table.js
@@ -1,12 +1,12 @@
 import React from 'react'
 
-function Table(props) {
-    return (
-      <table>
-          <TableHeader />
-          <TableBody characterData = {props.characterData} />
-      </table>
-    );  
+function Table (props) {
+  return (
+    <table>
+      <TableHeader />
+      <TableBody characterData={props.characterData} removeCharacter={props.removeCharacter} />
+    </table>
+  );
 }
 function TableHeader()  {
     return (
@@ -14,6 +14,7 @@ function TableHeader()  {
         <tr>
           <th>Name</th>
           <th>Job</th>
+          <th>Remove</th>
         </tr>
       </thead>
     );
@@ -22,8 +23,11 @@ function TableHeader()  {
     const rows = props.characterData.map((row, index) => {
       return (
         <tr key={index}>
-          <td>{row.name}</td>         
+          <td>{row.name}</td>
           <td>{row.job}</td>
+          <td>
+            <button onClick={() => props.removeCharacter(index)}>Delete</button>
+          </td>
         </tr>
       );
      }
